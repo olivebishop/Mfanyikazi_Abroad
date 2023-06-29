d4d0879dbc09444b4fcecfac75a2c01c881a0c60
olivebishop <olivehendrilgen1@gmail.com>
2023-06-29 05:33:52 +0300

    added boostrap link in app.js

--- src/components/PricingSection.jsx
+++ src/components/PricingSection.jsx
@@ -5,38 +5,52 @@ const PricingSection = () => {
   return (
     <section className="pricing-section bg-gray-100 py-10">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-8">Subscription Pricing</h2>
+        <h2 className="text-3xl font-bold text-center mb-8 text">
+          Subscription Pricing
+        </h2>
         <div className="flex flex-col md:flex-row justify-center items-center">
           <div className="flex-1 max-w-md mx-auto">
             <div className="bg-white rounded-lg shadow-lg p-6 mb-6">
-              <h3 className="text-2xl font-bold mb-4">Basic</h3>
+              <h3 className="text-2xl font-bold mb-4 text-black">Basic</h3>
               <p className="text-gray-600 mb-4">
-                Free service with real-time safety monitoring, secure communication channel & access to support
+                Free service with real-time safety monitoring, secure
+                communication channel & access to support
               </p>
-              <p className="text-3xl font-bold mb-4">Ksh1500/year</p>
-              <ul className="text-sm mb-4">
+              <p className="text-3xl font-bold mb-4 text-zinc-950">
+                Ksh1500/year
+              </p>
+              <ul className="text-sm mb-4 text-black">
                 <li>👉Real-time safety monitoring.</li>
                 <li>👉Secure communication channel.</li>
                 <li>👉Access to support.</li>
               </ul>
-              <Link to="/signup" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full no-underline">
+              <Link
+                to="/signup"
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full no-underline"
+              >
                 Get Started
               </Link>
             </div>
           </div>
           <div className="flex-1 max-w-md mx-auto">
             <div className="bg-white rounded-lg shadow-lg p-6">
-              <h3 className="text-2xl font-bold mb-4">Premium</h3>
+              <h3 className="text-2xl font-bold mb-4 text-black">Premium</h3>
               <p className="text-gray-600 mb-4">
-                Added features i.e. cultural awareness training, courses & enhanced customer service
+                Added features i.e. cultural awareness training, courses &
+                enhanced customer service
               </p>
-              <p className="text-3xl font-bold mb-4">Ksh3000/year</p>
-              <ul className="text-sm mb-4">
+              <p className="text-3xl font-bold mb-4 text-zinc-950">
+                Ksh3000/year
+              </p>
+              <ul className="text-sm mb-4 text-black">
                 <li>👉Cultural awareness training.</li>
                 <li>👉Enhanced customer service.</li>
                 <li>👉Courses</li>
               </ul>
-              <Link to="/signup" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full no-underline">
+              <Link
+                to="/signup"
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full no-underline"
+              >
                 Get Started
               </Link>
             </div>
@@ -47,7 +61,8 @@ const PricingSection = () => {
             * All prices are in Kenyan Shillings (Ksh) per year.
           </p>
           <p className="text-gray-600">
-            Recruitment agencies partnership available. Contact us for more details.
+            Recruitment agencies partnership available. Contact us for more
+            details.
           </p>
         </div>
       </div>
