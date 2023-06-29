1d96bd3c5eb60be61dcd753fb3a695c55281fd35
olivebishop <olivehendrilgen1@gmail.com>
2023-06-29 22:54:45 +0300

    added resposiveness in prising section

--- src/components/PricingSection.jsx
+++ src/components/PricingSection.jsx
@@ -5,12 +5,12 @@ const PricingSection = () => {
   return (
     <section className="pricing-section bg-gray-100 py-10">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-8 text">
+        <h2 className="text-3xl font-bold text-center mb-8">
           Subscription Pricing
         </h2>
-        <div className="flex flex-col md:flex-row justify-center items-center">
-          <div className="flex-1 max-w-md mx-auto">
-            <div className="bg-white rounded-lg shadow-lg p-6 mb-6">
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
+          <div className="max-w-md mx-auto">
+            <div className="bg-white rounded-lg shadow-lg p-6">
               <h3 className="text-2xl font-bold mb-4 text-black">Basic</h3>
               <p className="text-gray-600 mb-4">
                 Free service with real-time safety monitoring, secure
@@ -20,19 +20,19 @@ const PricingSection = () => {
                 Ksh1500/year
               </p>
               <ul className="text-sm mb-4 text-black">
-                <li>👉Real-time safety monitoring.</li>
-                <li>👉Secure communication channel.</li>
-                <li>👉Access to support.</li>
+                <li>👉 Real-time safety monitoring.</li>
+                <li>👉 Secure communication channel.</li>
+                <li>👉 Access to support.</li>
               </ul>
               <Link
                 to="/signup"
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full no-underline"
+                className="block mx-auto md:inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full no-underline"
               >
                 Get Started
               </Link>
             </div>
           </div>
-          <div className="flex-1 max-w-md mx-auto">
+          <div className="max-w-md mx-auto mt-4 sm:mt-0">
             <div className="bg-white rounded-lg shadow-lg p-6">
               <h3 className="text-2xl font-bold mb-4 text-black">Premium</h3>
               <p className="text-gray-600 mb-4">
@@ -43,13 +43,13 @@ const PricingSection = () => {
                 Ksh3000/year
               </p>
               <ul className="text-sm mb-4 text-black">
-                <li>👉Cultural awareness training.</li>
-                <li>👉Enhanced customer service.</li>
-                <li>👉Courses</li>
+                <li>👉 Cultural awareness training.</li>
+                <li>👉 Enhanced customer service.</li>
+                <li>👉 Courses.</li>
               </ul>
               <Link
                 to="/signup"
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full no-underline"
+                className="block mx-auto md:inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full no-underline"
               >
                 Get Started
               </Link>
