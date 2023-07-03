e73c815bc94f64e610db8cd1d6cdcdd5ab1e1eba
olivebishop <olivehendrilgen1@gmail.com>
2023-07-04 02:20:16 +0300

    edited forget file

--- src/components/ForgotPass.jsx
+++ src/components/ForgotPass.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import hero from "../assets/hero.jpg";
 import forgot from "../assets/forgot.svg";
 
 const ForgetPass = () => {
